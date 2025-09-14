ce5a84c569ec755b0424603dfd783641e5b547ea
Andre Bailey <AndreBailey442@gmail.com>
2025-09-14 15:04:59 +0000

    give a a white background with yellow tiles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="dark">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -23,7 +23,7 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body className={cn('font-sans antialiased', 'dark')}>
+      <body className={cn('font-sans antialiased')}>
         {children}
         <Toaster />
       </body>
